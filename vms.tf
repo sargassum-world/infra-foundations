5ad888cc577802079e26d92f33f4c9a53ac8b4ca
Ethan Li <lietk12@gmail.com>
2022-09-24 00:22:24 -0700

    Try to address defsec complaints

--- vms.tf
+++ vms.tf
@@ -13,25 +13,45 @@ provider "google" {
   image       = "alpine-virt-3.16.0"
   location    = "fsn1"
   datacenter  = "dc14"
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
 }*/
+
+resource "google_kms_key_ring" "foundations-1" {
+  name     = "foundations-1"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "foundations-1-1" {
+  name            = "foundations-1"
+  key_ring        = google_kms_key_ring.foundations-1
+  rotation_period = "7777000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 resource "google_compute_instance" "us-west1-a-1" {
   name         = "foundations-us-west1-a-1"
   machine_type = "e2-micro"
+  zone         = "us-west1-a"
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-minimal-2204-lts"
     }
+    disk_encryption_key_raw = google_kms_crypto_key.foundations-1-1
   }
 
   network_interface {
     network = "default"
-    access_config {
-    }
+  }
+
+  metadata = {
+    block-project-ssh-keys = true
+  }
+
+  shielded_instance_config {
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
   }
 }

64933ec03213c0c77907f9218998f41b354a3efd
Ethan Li <lietk12@gmail.com>
2022-09-23 19:45:13 -0700

    Refine the ZeroTier network

--- main.tf
+++
@@ -1,7 +0,0 @@
-provider "zerotier" {}
-
-resource "zerotier_network" "ztnet" {
-  name        = "infra.sargassum.world"
-  description = "Control plane for sargassum.world"
-  private     = true
-}
---
+++ networking.tf
@@ -0,0 +1,20 @@
+provider "zerotier" {}
+
+resource "zerotier_network" "ztoverlay" {
+  name        = "infra.sargassum.world"
+  description = "Control plane for sargassum.world"
+
+  assign_ipv4 {
+    zerotier = false
+  }
+
+  assign_ipv6 {
+    zerotier = false
+    sixplane = true
+    rfc4193  = false
+  }
+
+  private          = true
+  enable_broadcast = true
+  flow_rules       = file("${path.module}/ztoverlay-flowrules.txt")
+}

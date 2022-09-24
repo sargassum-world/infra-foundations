da31e202f7f4eb9b1911fc50e997b5c6d7cb232c
Ethan Li <lietk12@gmail.com>
2022-09-24 12:12:00 -0700

    Try to make a VPC and NAT for internet access (#6)
    * Try to make a VPC and NAT for internet access
    
    Signed-off-by: Ethan Li <lietk12@gmail.com>
    
    * Enable VPC flow logging to comply with defsec
    
    Signed-off-by: Ethan Li <lietk12@gmail.com>
    
    * Remove extraneous region field
    
    Signed-off-by: Ethan Li <lietk12@gmail.com>
    
    Signed-off-by: Ethan Li <lietk12@gmail.com>

--- vms-gcp.tf
+++ vms-gcp.tf
@@ -4,6 +4,46 @@ provider "google" {
   zone    = "us-west1-a"
 }
 
+resource "google_compute_network" "foundations" {
+  name = "foundations"
+}
+
+resource "google_compute_subnetwork" "foundations-us-west1" {
+  name          = "foundations-us-west1"
+  network       = google_compute_network.foundations.id
+  region        = "us-west1"
+  ip_cidr_range = "10.128.0.0/24"
+  
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_router" "us-west1" {
+  name    = "foundations-us-west1"
+  region  = "us-west1"
+  network = google_compute_network.foundations.id
+  
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "us-west1" {
+  name                               = "foundations-us-west1"
+  router                             = google_compute_router.us-west1.name
+  region                             = "us-west1"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 resource "google_kms_key_ring" "disks-1" {
   name     = "foundations-disks-1"
   location = "global"
@@ -38,8 +78,8 @@ resource "google_kms_crypto_key" "disk-1-1" {
 
 resource "google_compute_instance" "us-west1-a-1" {
   name         = "foundations-us-west1-a-1"
-  machine_type = "e2-micro"
   zone         = "us-west1-a"
+  machine_type = "e2-micro"
 
   boot_disk {
     initialize_params {
@@ -49,7 +89,7 @@ resource "google_compute_instance" "us-west1-a-1" {
   }
 
   network_interface {
-    network = "default"
+    subnetwork = google_compute_subnetwork.foundations-us-west1.id
   }
 
   metadata = {

e99c799aa69f27c13e4f8d925654806c632954f0
Ethan Li <lietk12@gmail.com>
2022-09-24 00:31:33 -0700

    Try provisioning a new free-tier GCP VM (#2)
    * Try provisioning a new free-tier GCP VM
    
    * Try to address defsec complaints
    
    * Fix error in specifying keyring
    
    * Reduce key rottion period to below 90 days
    
    * Fix use of the wrong schema item for the disk key

---
+++ vms.tf
@@ -0,0 +1,57 @@
+/*provider "hcloud" {
+}*/
+
+provider "google" {
+  project = "sargassum-world"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
+
+/*resource "hcloud_server" "eu-central-fsn1-dc14-1" {
+  name        = "foundations-eu-central-fsn1-dc14-1"
+  server_type = "cpx11"
+  image       = "alpine-virt-3.16.0"
+  location    = "fsn1"
+  datacenter  = "dc14"
+}*/
+
+resource "google_kms_key_ring" "disks-1" {
+  name     = "foundations-1"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "disk-1-1" {
+  name            = "foundations-1"
+  key_ring        = google_kms_key_ring.disks-1.id
+  rotation_period = "7770000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_compute_instance" "us-west1-a-1" {
+  name         = "foundations-us-west1-a-1"
+  machine_type = "e2-micro"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-minimal-2204-lts"
+    }
+    kms_key_self_link = google_kms_crypto_key.disk-1-1.id
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    block-project-ssh-keys = true
+  }
+
+  shielded_instance_config {
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+}

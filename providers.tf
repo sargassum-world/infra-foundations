03046d301d219ddac1efc1b824a0f110d1907deb
Ethan Li <lietk12@gmail.com>
2022-10-04 01:53:49 -0700

    Make the Nomad provider use the correct IP address

--- providers.tf
+++ providers.tf
@@ -9,5 +9,5 @@ provider "google" {
 }
 
 provider "nomad" {
-  address = "https://${google_compute_instance.us_west1_a_1.network_interface[0].network_ip}:4646"
+  address = "https://${google_compute_address.us_west1_a_1}:4646"
 }

bae859433cb6d10f055d90fea0e2b58fa61b696e
Ethan Li <lietk12@gmail.com>
2022-10-05 01:58:18 -0700

    Add Zerotier network ID to DNS records

--- desec-networking.tf
+++ desec-networking.tf
@@ -1,9 +1,17 @@
-resource "desec_domain" "foundations" {
+resource "desec_domain" "infra" {
   name = "infra.sargassum.world"
 }
 
+resource "desec_rrset" "zerotier" {
+  domain  = desec_domain.infra.name
+  subname = ""
+  type    = "TXT"
+  records = ["\"zerotier-net-id=${zerotier_network.foundations.id}\""]
+  ttl     = 3600
+}
+
 resource "desec_rrset" "gcp_us_west1_a_1_a" {
-  domain  = desec_domain.foundations.name
+  domain  = desec_domain.infra.name
   subname = "gcp-us-west1-a-1"
   type    = "A"
   records = [google_compute_address.us_west1_a_1.address]

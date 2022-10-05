a721b3a8f75807b27d70b260acc4271b2574207c
Ethan Li <lietk12@gmail.com>
2022-10-04 23:36:05 -0700

    Troubleshoot Nomad job with hello-world example

--- nomad-jobs.tf
+++ nomad-jobs.tf
@@ -1,9 +1,17 @@
+# resource "nomad_job" "zerotier_agent" {
+#   jobspec = templatefile("${path.module}/nomad-jobs/zerotier-agent.hcl.tftpl", {
+#     group       = "zerotier_agent_gcp_us_west1_a_1"
+#     private_key = zerotier_identity.gcp_us_west1_a_1.private_key
+#     public_key  = zerotier_identity.gcp_us_west1_a_1.public_key
+#   })
+
+#   hcl2 {
+#     enabled = true
+#   }
+# }
+#
 resource "nomad_job" "zerotier_agent" {
-  jobspec = templatefile("${path.module}/nomad-job-zerotier-agent.hcl.tftpl", {
-    group       = "zerotier_agent_gcp_us_west1_a_1"
-    private_key = zerotier_identity.gcp_us_west1_a_1.private_key
-    public_key  = zerotier_identity.gcp_us_west1_a_1.public_key
-  })
+  jobspec = file("${path.module}/nomad-jobs/hello-world.hcl")
 
   hcl2 {
     enabled = true
---
+++ nomad-jobs/hello-world.hcl
@@ -0,0 +1,24 @@
+job "docs" {
+
+  group "example" {
+    network {
+      port "http" {
+        static = "5678"
+      }
+    }
+    task "server" {
+      driver = "docker"
+
+      config {
+        image = "hashicorp/http-echo"
+        ports = ["http"]
+        args = [
+          "-listen",
+          ":5678",
+          "-text",
+          "hello world",
+        ]
+      }
+    }
+  }
+}

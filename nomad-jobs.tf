ccb80bd0c8888022a84dc6366bba26abc4b309d7
Ethan Li <lietk12@gmail.com>
2022-10-04 23:54:09 -0700

    Restore Nomad job for the Zerotier agent

--- nomad-jobs.tf
+++ nomad-jobs.tf
@@ -1,17 +1,9 @@
-# resource "nomad_job" "zerotier_agent" {
-#   jobspec = templatefile("${path.module}/nomad-jobs/zerotier-agent.hcl.tftpl", {
-#     group       = "zerotier_agent_gcp_us_west1_a_1"
-#     private_key = zerotier_identity.gcp_us_west1_a_1.private_key
-#     public_key  = zerotier_identity.gcp_us_west1_a_1.public_key
-#   })
-
-#   hcl2 {
-#     enabled = true
-#   }
-# }
-#
-resource "nomad_job" "hello_world" {
-  jobspec = file("${path.module}/nomad-jobs/hello-world.hcl")
+resource "nomad_job" "zerotier_agent" {
+  jobspec = templatefile("${path.module}/nomad-jobs/zerotier-agent.hcl.tftpl", {
+    group       = "zerotier_agent_gcp_us_west1_a_1"
+    private_key = zerotier_identity.gcp_us_west1_a_1.private_key
+    public_key  = zerotier_identity.gcp_us_west1_a_1.public_key
+  })
 
   hcl2 {
     enabled = true

ea1357fd15de93c8db28f7975acc4e3ad30f83e0
Ethan Li <lietk12@gmail.com>
2022-10-05 18:43:28 -0700

    Maanage desec records for sargassum.world

--- desec-networking.tf
+++ desec-networking.tf
@@ -1,3 +1,114 @@
+# Root
+#
+resource "desec_domain" "root" {
+  name = "sargassum.world"
+}
+
+resource "desec_rrset" "infra_ds" {
+  domain  = desec_domain.root.name
+  subname = "infra"
+  type    = "DS"
+  records = [
+    "23015 13 2 3ded016af87b9a6627d269d5b160e384d5f25b9daf84803b14122ddd1628661b",
+    "23015 13 4 1955d438ee865700d761d741d8a279dfc5ff1d488614cd70cea585b89fea2be1360333e7eb29f319acd95e55ac5ba94d",
+  ]
+  ttl = 3600
+}
+
+# Verification
+
+resource "desec_rrset" "sendgrid_cname" {
+  domain  = desec_domain.root.name
+  subname = "em7308"
+  type    = "CNAME"
+  records = ["u29124363.wl114.sendgrid.net."]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "sendgrid1_cname" {
+  domain  = desec_domain.root.name
+  subname = "s1._domainkey"
+  type    = "CNAME"
+  records = ["s1.domainkey.u29124363.wl114.sendgrid.net."]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "sendgrid2_cname" {
+  domain  = desec_domain.root.name
+  subname = "s2._domainkey"
+  type    = "CNAME"
+  records = ["s2.domainkey.u29124363.wl114.sendgrid.net."]
+  ttl     = 3600
+}
+
+# Unmigrated Services
+
+resource "desec_rrset" "fluitans_caprover_a" {
+  domain  = desec_domain.root.name
+  subname = "*.cloud.fluitans"
+  type    = "A"
+  records = ["49.12.214.144"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "fluitans_caprover_aaaa" {
+  domain  = desec_domain.root.name
+  subname = "*.cloud.fluitans"
+  type    = "AAAA"
+  records = ["2a01:4f8:c010:a153::"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "syngnathus_caprover_a" {
+  domain  = desec_domain.root.name
+  subname = "*.cloud.syngnathus"
+  type    = "A"
+  records = ["35.212.249.95"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "fluitans_a" {
+  domain  = desec_domain.root.name
+  subname = "fluitans"
+  type    = "A"
+  records = ["49.12.214.144"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "fluitans_aaaa" {
+  domain  = desec_domain.root.name
+  subname = "fluitans"
+  type    = "AAAA"
+  records = ["2a01:4f8:c010:a153::"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "live_a" {
+  domain  = desec_domain.root.name
+  subname = "live"
+  type    = "A"
+  records = ["35.212.249.95"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "fider_a" {
+  domain  = desec_domain.root.name
+  subname = "fider"
+  type    = "A"
+  records = ["49.12.214.144"]
+  ttl     = 3600
+}
+
+resource "desec_rrset" "fider_aaaa" {
+  domain  = desec_domain.root.name
+  subname = "fider"
+  type    = "AAAA"
+  records = ["2a01:4f8:c010:a153::"]
+  ttl     = 3600
+}
+
+# Infra
+
 resource "desec_domain" "infra" {
   name = "infra.sargassum.world"
 }

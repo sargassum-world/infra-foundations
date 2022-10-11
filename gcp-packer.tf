425847ec06ebabf727a721771535ee2413df778c
Ethan Li <lietk12@gmail.com>
2022-10-11 00:31:36 -0700

    Make service account for the Nomad CSI plugin for GCS

--- gcp-packer.tf
+++ gcp-packer.tf
@@ -4,12 +4,20 @@ resource "google_service_account" "packer" {
   description  = "Build VM images"
 }
 
+# Note: the Terraform service account must be given the Project IAM Admin role or another role with
+# the resourcemanager.projects.setIamPolicy permission, such as the Security Admin role, so that it
+# can manage the roles for GCP's own service accounts. You can manually do this from the IAM & Admin
+# console.
 resource "google_project_iam_member" "compute_service_pkr" {
   project = google_project.foundations.project_id
   role    = "roles/compute.instanceAdmin.v1"
   member  = "serviceAccount:${google_service_account.packer.email}"
 }
 
+# Note: the Terraform service account must be given the Project IAM Admin role or another role with
+# the resourcemanager.projects.setIamPolicy permission, such as the Security Admin role, so that it
+# can manage the roles for GCP's own service accounts. You can manually do this from the IAM & Admin
+# console.
 resource "google_project_iam_member" "iap_service_pkr" {
   project = google_project.foundations.project_id
   role    = "roles/iap.tunnelResourceAccessor"
--- gcp-storage-orchestrators.tf
+++ gcp-storage-orchestrators.tf
@@ -1,3 +1,21 @@
+# Service Account
+#
+resource "google_service_account" "nomad_plugin_csi_gcs" {
+  account_id   = "nomad-plugin-csi-gcs"
+  display_name = "nomad-plugin-csi-gcs"
+  description  = "Manage GCS buckets for Nomad workload volumes"
+}
+
+# Note: the Terraform service account must be given the Project IAM Admin role or another role with
+# the resourcemanager.projects.setIamPolicy permission, such as the Security Admin role, so that it
+# can manage the roles for GCP's own service accounts. You can manually do this from the IAM & Admin
+# console.
+resource "google_project_iam_member" "storage_service_nomad_plugin_csi" {
+  project = google_project.foundations.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.nomad_plugin_csi_gcs.email}"
+}
+
 # Volumes
 
 resource "google_storage_bucket" "foundations_caddy_public_server_data" {

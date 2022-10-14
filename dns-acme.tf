1bb123ffe386db0656e289110743e5983465e95d
Ethan Li <lietk12@gmail.com>
2022-10-13 18:44:13 -0700

    Try to regenerate the ACME registration, too

--- dns-acme.tf
+++ dns-acme.tf
@@ -1,9 +1,9 @@
-resource "tls_private_key" "dns_acme" {
+resource "tls_private_key" "acme" {
   algorithm = "RSA"
 }
 
 resource "acme_registration" "main" {
-  account_key_pem = tls_private_key.dns_acme.private_key_pem
+  account_key_pem = tls_private_key.acme.private_key_pem
   email_address   = var.acme_email
 }
 

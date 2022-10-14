4ad9fb83d05f5e8e937c3c4843f1e0e4ec00ecf6
Ethan Li <lietk12@gmail.com>
2022-10-13 20:47:27 -0700

    Evaluate Caddyfile template with  Terraform's before passing to Nomad

--- nomad-jobs.tf
+++ nomad-jobs.tf
@@ -19,20 +19,17 @@ resource "nomad_job" "zerotier_agent" {
   }
 }
 
-data "local_file" "caddy_caddyfile" {
-  filename = "${path.module}/nomad-jobs/caddy_Caddyfile.tpl.tftpl"
-}
-
 resource "nomad_job" "caddy" {
   jobspec = templatefile("${path.module}/nomad-jobs/caddy.hcl.tftpl", {
     group                 = "gcp_us_west1_a_1"
     affinity              = google_compute_instance.us_west1_a_1.name
-    caddyfile             = data.local_file.caddy_caddyfile.content
-    acme_email            = var.acme_email
-    root_domain           = desec_domain.root
-    device_subname        = "gcp-us-west1-a-1"
     ztoverlay_certificate = acme_certificate.zerotier_wildcards.certificate_pem
     ztoverlay_private_key = acme_certificate.zerotier_wildcards.private_key_pem
+    caddyfile = templatefile("${path.module}/nomad-jobs/caddy_Caddyfile.tpl.tftpl", {
+      root_domain    = desec_domain.root.name
+      device_subname = "gcp-us-west1-a-1"
+      acme_email     = var.acme_email
+    })
   })
 
   hcl2 {
